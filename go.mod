9c9ee7fa5efd42af75d87644007aeff52b05b4d2
Mohcha998 <mohcha998@gmail.com>
2023-08-18 13:57:36 +0700

    major changes

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Mohcha998/helper
+module github.com/Mohcha998/helper/v2
 
 go 1.21.0
--- hello.go
+++ hello.go
@@ -1,5 +1,5 @@
 package helper
 
 func SayHello(name string) string {
-	return "Hello" + name
+	return "Hello " + name
 }
